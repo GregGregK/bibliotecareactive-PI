dc2c356baf496ec7a68086d6969de6e6004f0972
Gregori R <gregori_monteiro@estudante.sc.senai.br>
2023-06-13 13:29:35 -0300

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
+import Login from "./components/Login"
 
 function App() {
-  return <div className="App">Greg </div>
+  return(
+    <div>
+      <Login /> 
+    </div>
+  ) 
   
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import loginImg from '../assets/Library 1.png'
+
+
+export default function Login(){
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2'>
+            <div>
+                <img src={loginImg} alt="" />
+            </div>
+            
+            <div>
+                <form>
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
